e8ef1076bfb63af9d2a190952e335be20d1aed12
MTUL <morten.tulstrup@gmail.com>
2024-05-31 12:46:00 +0200

    Update launch_simulation_script.sh

--- launch_simulation_script.sh
+++ launch_simulation_script.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+ne#!/bin/sh
 #PBS -W group_list=cu_10184 -A cu_10184
 #PBS -m n
 #PBS -l nodes=1:ppn=1
@@ -10,4 +10,4 @@ module load gcc
 module load intel/perflibs
 module load R/4.1.0
 
-Rscript /home/people/morytu/scripts/EVI2_EPICarray/simulation_script_stratified.R $1 $2 $3 $4 $5 $6
+Rscript /home/people/morytu/scripts/EVI2_EPICarray/simulation_script_general.R $1 $2 $3 $4 $5 $6
